7d587f75cb123b7fe0bb5aa9790a93900b2ea709
meisnatta <nataliapoddueva435@gmail.com>
2025-09-14 20:56:20 +0300

    java.java

--- java.java
+++ java.java
@@ -1,1 +1,23 @@
+import java.util.ArrayList;
+import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.HashMap;
 
+public class Main {
+    public static void main(String[] args) {
+        // Создание списка
+        ArrayList<String> list = new ArrayList<>();
+        list.add("элемент1");
+        list.add("элемент2");
+        list.add("элемент3");
+        System.out.println("Список: " + list);
+       // Организация стека
+      stack<string> st;
+    st.push("элемент1");
+    st.push("элемент2");
+    cout << "Вершина стека: " << st.top() << endl;
+    st.pop();
+    cout << "После извлечения: " << st.top() << endl;    
+    }
+}
