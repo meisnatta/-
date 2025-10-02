fe28bd7ace2c6ebd43e8464db0648f39cdcb89c4
meisnatta <nataliapoddueva435@gmail.com>
2025-10-02 18:17:34 +0300

    Update hash.java

--- hash.java
+++ hash.java
@@ -1,1 +1,18 @@
+import java.util.HashMap;
 
+public class SimpleExample {
+    public static void main(String[] args) {
+        // Создаем хеш-таблицу
+        HashMap<String, Integer> myMap = new HashMap<>();
+        
+        // Добавляем элементы
+        myMap.put("apple", 10);
+        myMap.put("banana", 20);
+        myMap.put("orange", 15);
+        
+        // Выводим все элементы
+        for (String fruit : myMap.keySet()) {
+            System.out.println(fruit + ": " + myMap.get(fruit));
+        }
+    }
+}
